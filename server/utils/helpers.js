1f18dcd742359b6eb8156dfc37890089fa2dc550
unknown <tenorio2003@gmail.com>
2024-10-24 11:16:35 -0500

    add_functionsScaff

--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -29,10 +29,32 @@ const marcoScore = (winner) => {
   const marcoWin = 50
   const marcoLose = -10
 
-  if(winner == "polo"){
+  if(winner == "marco"){
     return marcoWin;
   } else{}
     return marcoLose;
+}
+
+const endGameByScore = (winner) => {
+  if (poloSelected.role === "polo-especial") {
+    db.players.forEach((element) => {
+      io.to(element.id).emit("notifyGameOver", {
+        message: `El marco ${myUser.nickname} ha ganado, ${poloSelected.nickname} ha sido capturado`,
+      })
+    })
+  } else {
+    db.players.forEach((element) => {
+      io.to(element.id).emit("notifyGameOver", {
+        message: `El marco ${myUser.nickname} ha perdido`,
+      })
+    })
+  }
+}
+
+const scoreEvaluator3000 = () => {
+}
+
+const ultraComplexSortingAlgorithm = () => {
 }
 
 module.exports = { assignRoles, assignScore }
